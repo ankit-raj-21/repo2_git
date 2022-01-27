d3896f811a7a7f51fadc0a44a8261c7be16de507
ankit <ankitraj.21.91@gmail.com>
2022-01-27 15:33:40 +0530

    password added

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # This is a sample Python script.
-
+from password import password
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
@@ -15,5 +15,6 @@ if __name__ == '__main__':
 
     print('how are you')
     print('New line')
+    print(password)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
