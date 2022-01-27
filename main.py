b907839b06fef531e90213f5d0fc6567e7862bde
ankit <ankitraj.21.91@gmail.com>
2022-01-27 15:45:53 +0530

    new add function added

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
+def add(a,b):
+    return a+b
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Ankit')
@@ -16,5 +17,6 @@ if __name__ == '__main__':
     print('how are you')
     print('New line')
     print(password)
+    print(add(3,4))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
